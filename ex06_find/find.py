54fa85e2e50166374c2398fe8143bed2d865f7d2
LokiSharp <loki.sharp@gmail.com>
2017-09-16 23:31:20 +0800

    Update: ex06 find - Fix some bugs and add the -print argument

--- ex06_find/find.py
+++ ex06_find/find.py
@@ -4,27 +4,34 @@ from pathlib import Path
 
 def find(args):
     start_path=Path(args.start[0])
-    #-name
+
+    #If -name
     if args.name and not args.type:
         for f in start_path.rglob(args.name):
-            print(f)
-    #-type
-    if args.type:
+            find_print(f, args)
+
+    #If -type
+    elif args.type:
         if args.type in ['d','f']:
             for f in start_path.rglob(args.name or '*'):
                 if args.type == 'd' and f.is_dir():
-                    print(f)
+                    find_print(f, args)
                 elif args.type == 'f' and f.is_file():
-                    print(f)
+                    find_print(f, args)
+
         else:
             print(f'Unknow type: {args.type}')
-    #All of file
+
+    #Else All of file
     else:
         for f in start_path.rglob('*'):
             print(f)
 
-
-
+def find_print(f, args):
+    if args.print:
+        print(f"./{f}")
+    else:
+        print(f)
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -33,7 +40,6 @@ def parse_args():
     parser.add_argument('-name', type=str)
     parser.add_argument('-type', type=str)
     parser.add_argument('-print', action='store_true')
-
 
     return parser.parse_args()
 
