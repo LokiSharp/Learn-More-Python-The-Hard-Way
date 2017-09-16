d6f278ab5f742e75b2fb0e67f7919e3aba0f6389
LokiSharp <loki.sharp@gmail.com>
2017-09-17 01:33:28 +0800

    Update: ex06 find - Add the -exec argument

--- ex06_find/find.py
+++ ex06_find/find.py
@@ -1,6 +1,8 @@
+import os
+import glob
+import subprocess
 import argparse
 from pathlib import Path
-
 
 def find(args):
     start_path=Path(args.start[0])
@@ -8,24 +10,23 @@ def find(args):
     #If -name
     if args.name and not args.type:
         for f in start_path.rglob(args.name):
-            find_print(f, args)
+            find_exec(f, args)
 
     #If -type
     elif args.type:
         if args.type in ['d','f']:
             for f in start_path.rglob(args.name or '*'):
                 if args.type == 'd' and f.is_dir():
-                    find_print(f, args)
+                    find_exec(f, args)
                 elif args.type == 'f' and f.is_file():
-                    find_print(f, args)
-
+                    find_exec(f, args)
         else:
             print(f'Unknow type: {args.type}')
 
     #Else All of file
     else:
         for f in start_path.rglob('*'):
-            print(f)
+            find_exec(f, args)
 
 def find_print(f, args):
     if args.print:
@@ -33,12 +34,23 @@ def find_print(f, args):
     else:
         print(f)
 
+def find_exec(f, args):
+    if args.exec:
+        command = [w.replace('{}', str(f)) for w in args.exec]
+        command.pop()
+        completed = subprocess.run(command)
+
+    else:
+        find_print(f, args)
+
+
 def parse_args():
     parser = argparse.ArgumentParser()
 
     parser.add_argument('start', type=str, nargs=1)
     parser.add_argument('-name', type=str)
     parser.add_argument('-type', type=str)
+    parser.add_argument('-exec', type=str, nargs="+")
     parser.add_argument('-print', action='store_true')
 
     return parser.parse_args()
